57284a82d596b7df5ecd9a3b8cc5d68039bd2ae2
Oren Anderson <42502680+Raviolifun@users.noreply.github.com>
2020-06-23 02:45:52 -0400

    Comments
    Added comments to the link class and changed its rotation to ccw being postive rather than cw

--- Link.py
+++ Link.py
@@ -10,44 +10,60 @@ from pyglet import shapes
 
 class Link:
     def __init__(self, x, y, length, color1=(255, 255, 255), color2=(200, 200, 200), color3=(200, 200, 200), batch=None, group=None):
+        # (x, y) describes the position of the fixed circle
         self._x = x
         self._y = y
+        # length describes the length from center to center
         self._length = length
-        # Angle from negative vertical (degrees)
+        # Counterclockwise angle from negative vertical (degrees)
         self._rotation = 0
 
-        # Outline
+        # Outline shapes
+        # Set up rectangle in center
         self.rectangle = shapes.Rectangle(x=x-length/20, y=y, width=length/10, height=length,
                                           color=color1, batch=batch, group=group)
+        # Anchor rectangle to top middle (allows for rotation about a convenient point)
         self.rectangle._anchor_x = self.rectangle.width / 2
         self.rectangle._anchor_y = self.rectangle.height
+        # Set up circles at tips
         self.circleOne = shapes.Circle(x=x, y=y, radius=length/10, color=color1, batch=batch, group=group)
         self.circleTwo = shapes.Circle(x=x, y=y+length, radius=length/10, color=color1, batch=batch, group=group)
 
-        # Inner
+        # Inner shapes
+        # Set up rectangle in center
         self.rectangleIn = shapes.Rectangle(x=x-length/32, y=y, width=length/16, height=length,
                                             color=color3, batch=batch, group=group)
+        # Anchor rectangle to top middle (allows for rotation about a convenient point)
         self.rectangleIn._anchor_x = self.rectangleIn.width / 2
         self.rectangleIn._anchor_y = self.rectangleIn.height
+        # Set up circles at tips
         self.circleOneIn = shapes.Circle(x=x, y=y, radius=length/12, color=color2, batch=batch, group=group)
         self.circleTwoIn = shapes.Circle(x=x, y=y+length, radius=length/12, color=color2, batch=batch, group=group)
 
     def _update_position(self):
+        # Updates position of outer
+        # Move fixed circle
         self.circleOne.x = self._x
         self.circleOne.y = self._y
-        self.circleTwo.x = self._x - math.sin(self._rotation * math.pi / 180) * self._length
+        # Move moving circle (linkage end)
+        self.circleTwo.x = self._x + math.sin(self._rotation * math.pi / 180) * self._length
         self.circleTwo.y = self._y - math.cos(self._rotation * math.pi / 180) * self._length
+        # Rotate rectangle
         self.rectangle.x = self._x
         self.rectangle.y = self._y
-        self.rectangle.rotation = self._rotation
+        self.rectangle.rotation = -self._rotation
 
+        # Updates position of inner
+        # Move fixed circle
         self.circleOneIn.x = self._x
         self.circleOneIn.y = self._y
-        self.circleTwoIn.x = self._x - math.sin(self._rotation * math.pi / 180) * self._length
+        # Move moving circle (linkage end)
+        self.circleTwoIn.x = self._x + math.sin(self._rotation * math.pi / 180) * self._length
         self.circleTwoIn.y = self._y - math.cos(self._rotation * math.pi / 180) * self._length
+        # Rotate rectangle
         self.rectangleIn.x = self._x
         self.rectangleIn.y = self._y
-        self.rectangleIn.rotation = self._rotation
+        self.rectangleIn.rotation = -self._rotation
 
     @property
     def length(self):
