1b5a43a188681cb3f355d5e4679d6426440bc20b
Oren Anderson <42502680+Raviolifun@users.noreply.github.com>
2020-06-28 19:14:03 -0400

    Added a Generalized Pendulum
    Added another pendulum class. This new one springs, has air resistance, and can be moved by the curser with realistic impact on the pendulum motion.

--- MainGeneralSpringPendulum.py
+++ MainGeneralSpringPendulum.py
@@ -1,4 +1,4 @@
-# Model for a basic pendulum
+# Model for generalized spring pendulum with wind resistance
 
 # Standard
 import math
@@ -23,6 +23,10 @@ icon2 = pyglet.image.load('.\\resources\\Bee32.png')
 window.set_icon(icon1, icon2)
 fps_display = pyglet.window.FPSDisplay(window=window)
 
+# For size adjustment
+pixelsPerMeter = 100
+xPos = window.get_size()[0] / 2
+yPos = window.get_size()[1] / 2
 
 # Required for updating the screen
 @window.event
@@ -32,11 +36,13 @@ def on_draw():
     # fps_display.draw()
 
 
-# Key event example to move window
+# Moving the link
 @window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
-    if buttons & mouse.LEFT:
-        pass
+    global xPos, yPos
+    if buttons & mouse.LEFT & (((-link.x + x)**2 + (-link.y + y)**2)**0.5 < 500):
+        xPos = x
+        yPos = y
 
 
 # Function for centering objects
@@ -46,9 +52,6 @@ def center_image(image):
     image.anchor_y = image.height // 2
 
 
-# For size adjustment
-pixelsPerMeter = 100
-
 # Batch of all objects (renders them in one go)
 main_batch = pyglet.graphics.Batch()
 
@@ -57,21 +60,38 @@ motionFade = MotionFade.MotionFade(x=window.get_size()[0] / 2, y=window.get_size
                                    color1=(40, 105, 215), batch=main_batch)
 link = Link.Link(x=window.get_size()[0] / 2, y=window.get_size()[1] * 4 / 8, length=200, color1=(225, 225, 225),
                  color2=(100, 100, 100), color3=(140, 140, 140), batch=main_batch)
+# linka = Link.Link(x=window.get_size()[0] / 2, y=window.get_size()[1] * 4 / 8, length=200, color1=(225, 225, 225),
+#                  color2=(100, 100, 100), color3=(140, 140, 140), batch=main_batch)
 graph = Graph.MovingGraph(x=100, y=10, width=200, height=100, colors=((0, 0, 255), (0, 255, 0)), batch=main_batch)
 
 # Function Constants
 g = 9.81
-l0 = 3
-m = 2
-k = 100
+l0 = 2
+m = 1
+k = 1000
+p = 1.225
+A = 1
+cd = 0.47
 
 # Initialisation Constants
-th = math.pi + 0.0000001
+th = math.pi/2+0.0000001
 thd = 0
 thdd = 0
 xx = 0
 xxd = 0
 xxdd = 0
+
+xPosf = xPos
+yPosf = yPos
+vxf = 0
+vyf = 0
+
+vx = 0
+vy = -10
+ax = 0
+ay = -10
+vxr = 0
+vyr = 0
 time = 0
 
 
@@ -79,16 +99,23 @@ def update(dt):
     # The Dynamic System: is a double pendulum
 
     # Force these to be global rather than local
-    global th, thd, thdd, xx, xxd, xxdd, time
-
-    # Internal simulation loop for more accurate performance
+    global th, thd, thdd, xx, xxd, xxdd, vxr, vyr, time
+    global xPosf, xPos, yPosf, yPos, vxf, vx, vyf, vy, ax, ay
 
     dtT = dt
     dtF = 1/10000
     while dtT > dtF:
 
-        thdd = (g * math.sin(th) + thd * xxd) / -(l0 + xx)
-        xxdd = (m * thd**2 * (xx + l0) + m * g * math.cos(th) - k * xx) / m
+        vyy = -vy + thd * (l0 + xx) * math.sin(th) + vyr - xxd * math.cos(th)
+        vxx = -vx + thd * (l0 + xx) * math.cos(th) + vxr + xxd * math.sin(th)
+        ps = -math.atan2(vyy, vxx)
+        VV2 = vxx ** 2 + vyy ** 2
+        R2 = 1 / 2.0 * p * A * cd * VV2
+
+        thdd = (m * g * math.sin(th) + m * thd * xxd + R2 * math.cos(ps + th) + m * ax * math.cos(
+            th) + m * ay * math.sin(th)) / -(l0 * m + xx * m)
+        xxdd = (m * thd ** 2 * (xx + l0) + m * g * math.cos(th) - k * xx - R2 * math.sin(ps + th) - m * ax * math.sin(
+            th) + m * ay * math.cos(th)) / m
 
         thd = thd + thdd * dtF
         th = th + thd * dtF
@@ -99,26 +126,51 @@ def update(dt):
         # Give graph information, bottom right corner
         time += dtF
 
-    thdd = (g * math.sin(th) + thd * xxd) / -(l0 + xx)
-    xxdd = (m * thd ** 2 * (xx + l0) + m * g * math.cos(th) - k * xx) / m
+    vyy = -vy + thd * (l0 + xx) * math.sin(th) + vyr - xxd * math.cos(th)
+    vxx = -vx + thd * (l0 + xx) * math.cos(th) + vxr + xxd * math.sin(th)
+    ps = -math.atan2(vyy, vxx)
+    VV2 = vxx ** 2 + vyy ** 2
+    R2 = 1 / 2.0 * p * A * cd * VV2
+
+    thdd = (m * g * math.sin(th) + m * thd * xxd + R2 * math.cos(ps + th) + m * ax * math.cos(
+        th) + m * ay * math.sin(th)) / -(l0 * m + xx * m)
+    xxdd = (m * thd ** 2 * (xx + l0) + m * g * math.cos(th) - k * xx - R2 * math.sin(ps + th) - m * ax * math.sin(
+        th) + m * ay * math.cos(th)) / m
 
     thd = thd + thdd * dtF
     th = th + thd * dtF
     xxd = xxd + xxdd * dtF
     xx = xx + xxd * dtF
 
+    # This is for the mouse interaction code
+    vx = (xPosf - xPos)/(pixelsPerMeter * dt)
+    vy = (yPosf - yPos)/(pixelsPerMeter * dt)
+    ax = (vxf - vx)/dt
+    ay = (vyf - vy)/dt
+
     graph.update_graph(time, thd/4 + 4)
     graph.update_graph(time, xxd/4 + 4)
     time += dt
 
-    link.x = window.get_size()[0] / 2
-    link.y = window.get_size()[1] * 4 / 8
+    # for mouse velocity calcs
+    xPosf = xPos
+    yPosf = yPos
+    vxf = vx
+    vyf = vy
+
+    # on resize:
+    # link.x = window.get_size()[0] / 2
+    # link.y = window.get_size()[1] * 4 / 8
+    # else:
+    link.x = xPos
+    link.y = yPos
+
     link.rotation = th * 180 / math.pi
     link.length = pixelsPerMeter * (l0 + xx)
 
-    enr = 1/2.0 * k * xx**2 + 1/2.0 * m * (xxd**2 + (thd * (l0 + xx))**2) - m * g * math.cos(th) * (l0 + xx)
-
-    print(enr)
+    # linka.x = link.x2
+    # linka.y = link.y2
+    # linka.rotation = (-ps) * 180 / math.pi + 90
 
     # Add a motion Fade to the end of the linkage
     motionFade.pos = [link.x2, link.y2]
